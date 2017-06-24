eaa0fda6eef55579edfd3e8b9d6dbb18b5ed96ed
Apurva Nitanjay <apuayush@gmail.com>
2017-06-25 00:04:54 +0530

    basic structuring

---
+++ main.py
@@ -0,0 +1,35 @@
+# Tornado libraries
+from tornado.ioloop import IOLoop
+from tornado.web import RequestHandler, Application
+from tornado.httpserver import HTTPServer
+
+# other libraries
+from pymongo import MongoClient
+import os
+import env
+
+db = MongoClient(env.DB_Link)['chatbot-learn']
+ques = db['ques']
+
+class MainHandler(RequestHandler):
+    def get(self):
+        ques_count = db.ques.count()
+        self.render('index.html', ques_count = ques_count)
+
+settings = dict(
+    db=db,
+    debug=True
+)
+
+app = Application(
+    handlers=[
+        (r'/leaderboard',MainHandler)
+    ],
+    template_path=os.path.join(os.path.dirname(__file__), "template"),
+    static_path=os.path.join(os.path.dirname(__file__), "static"),
+    **settings)
+
+if __name__=="__main__":
+    server = HTTPServer(app)
+    server.listen(8555)
+    IOLoop.current().start()
