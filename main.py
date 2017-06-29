95e233e801cc66d9f83826e09e08c411c379eed6
Apurva Nitanjay <apuayush@gmail.com>
2017-06-30 02:21:00 +0530

    ques-question error

--- main.py
+++ main.py
@@ -22,10 +22,10 @@ class MainHandler(RequestHandler):
 
         if db.ques.find_one({'ques':ques}) != None :
              self.write('such a question exist')
-        elif len(ques) or len(ans) < 10:
+        elif len(ques) <10 or len(ans) < 10:
             self.write('your answer or question is too short..please try again')
         else :
-            db.ques.update({'ques': ques},
+            db.ques.update({'question': ques},
                             {"$set": {'question': ques,
                                       'answer': ans}}, upsert=True)
             self.write("your answer was submitted successfully")
