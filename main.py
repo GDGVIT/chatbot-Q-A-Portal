0e8223fd11c0a7665ded36a043fa835e265ab17b
shubham0704 <shubham.bhardwaj2015@vit.ac.in>
2017-07-02 20:56:24 +0530

    made required changes

--- main.py
+++ main.py
@@ -2,33 +2,44 @@
 from tornado.ioloop import IOLoop
 from tornado.web import RequestHandler, Application
 from tornado.httpserver import HTTPServer
+from tornado.gen import coroutine, sleep
 
 # other libraries
-from pymongo import MongoClient
+from motor import MotorClient
 import os
 import env
 
-db = MongoClient(env.DB_Link)['chatbot-learn']
+db = MotorClient(env.DB_Link)['chatbot-learn']
 ques = db['ques']
 
+
 class MainHandler(RequestHandler):
+    @coroutine
     def get(self):
         ques_count = db.ques.count()
-        self.render('index.html', ques_count = ques_count)
+        try:
+            old_response = self.get_query_argument('response')
+            response = "your answer was submitted successfully, try another"
+        except:
+            response = ''
+        self.render('index.html', ques_count=ques_count, response=response)
 
+    @coroutine
     def post(self):
+        ques_type = self.get_argument('ques_type').lower()
         ques = self.get_argument("ques").rstrip('?').strip()
         ans = self.get_argument("ans")
 
-        if db.ques.find_one({'ques':ques}) != None :
-             self.write('such a question exist')
-        elif len(ques) <10 or len(ans) < 10:
+        check_inside = yield db.ques.find_one({'ques': ques})
+        if check_inside is not None:
+            self.write('such a question exist')
+        elif len(ques) < 10 or len(ans) < 10:
             self.write('your answer or question is too short..please try again')
-        else :
-            db.ques.update({'question': ques},
-                            {"$set": {'question': ques,
-                                      'answer': ans}}, upsert=True)
-            self.write("your answer was submitted successfully")
+        else:
+            yield db.ques.update({'question': ques},
+                                 {"$set": {'question': ques, 'answer': ans,
+                                  'type': ques_type}}, upsert=True)
+            self.redirect('/?response=True')
 
 
 settings = dict(
@@ -38,13 +49,13 @@ settings = dict(
 
 app = Application(
     handlers=[
-        (r'/',MainHandler)
+        (r'/', MainHandler)
     ],
     template_path=os.path.join(os.path.dirname(__file__), "template"),
     static_path=os.path.join(os.path.dirname(__file__), "static"),
     **settings)
 
-if __name__=="__main__":
+if __name__ == "__main__":
     server = HTTPServer(app)
-    server.listen(os.environ.get("PORT",5000))
+    server.listen(os.environ.get("PORT", 5000))
     IOLoop.current().start()
